68342f06420916bbbda36c5ec47ba5be93d1b734
adomaco <101427389+adomaco@users.noreply.github.com>
2023-10-01 11:44:08 -0600

    Added UI to GamePlay Scene
    Added left and right buttons on the screen (non-functional)
    Added functionality so pressing left or right keys changes the scene accordingly.
    Added bug sprite and it's corresponding animations.
    Added score counter that goes up by one everytime you press the up button
    Slightly better transitions

---
+++ Bug Game/Assets/ShowScore.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShowScore : MonoBehaviour
+{
+    public string textValue = "Score: ";
+    public int scoreValue = 0;
+    public TextMeshProUGUI textElement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textElement.text = textValue + scoreValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKey("up"))
+		{
+            scoreValue++;
+			textElement.text = textValue + scoreValue;
+		}
+	}
+}
