a848491b7679c6106c5ec578b2178c7462a820ea
yronk9 <geronay@mymacewan.ca>
2023-10-05 15:53:01 -0600

    Touch added to title

--- Bug Game/Assets/Scripts/TitleButton.cs
+++ Bug Game/Assets/Scripts/TitleButton.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TouchScript.Gestures;
 using UnityEngine.SceneManagement;
 
 public class TitleButton : MonoBehaviour
 {
-    public void GoTut() 
+    // Start is called before the first frame update
+    public void GoTut()
     {
+        TapGesture tapGesture = this.GetComponent<TapGesture> () ;
+        tapGesture.Tapped += TapGesture_Tapped ;
+    }
+
+    
+    void TapGesture_Tapped (object sender, System.EventArgs e) {
         SceneManager.LoadScene("TutorialScene");
     }
 }
