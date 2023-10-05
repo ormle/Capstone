8710e99af1d20168b4eeafad3381b6e07f1bc9ea
nn4ncyy <dinhn2@mymacewan.ca>
2023-10-04 20:55:04 -0600

    Update HomeButton.cs
    Updated home button to be touch responsive rather than click responsive.

--- Bug Game/Assets/Scripts/HomeButton.cs
+++ Bug Game/Assets/Scripts/HomeButton.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TouchScript.Gestures;
 using UnityEngine.SceneManagement;
 
 public class HomeButton : MonoBehaviour
 {
-    public void GoHome() 
+    // Start is called before the first frame update
+    void Start()
     {
+        TapGesture tapGesture = this.GetComponent<TapGesture> () ;
+        tapGesture.Tapped += TapGesture_Tapped ;
+    }
+
+    
+    void TapGesture_Tapped (object sender, System.EventArgs e) {
         SceneManager.LoadScene("TitleScene");
     }
 }
