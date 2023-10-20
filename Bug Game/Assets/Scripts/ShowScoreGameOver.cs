963e43b180a9c82d44933f0c4461ac4e9281de1b
yronk9 <geronay@mymacewan.ca>
2023-10-19 23:37:57 -0600

    additional files

--- Bug Game/Assets/Scripts/ShowScoreGameOver.cs
+++ Bug Game/Assets/Scripts/ShowScoreGameOver.cs
@@ -7,36 +7,26 @@ using TMPro;
 public class ShowScoreGameOver : MonoBehaviour
 {
     public string textValue = "You caught ";
-
     public string bugsTXT = " bugs!";
-    public int scoreValue = 0;
     public TextMeshProUGUI textElement;
 
     // Start is called before the first frame update
     void Start()
     {
-        // textElement.text = textValue + scoreValue + " " + bugsTXT;
-	
-	// finds the ShowScore script in the scene
-	ShowScore scoreScript = GameObject.FindObjectOfType<ShowScore>();
-    
-	if (scoreScript != null)
-	{
-	    // gets the score value from the ShowScore script
-	    scoreValue = scoreScript.scoreValue;
-	    textElement.text = textValue + scoreValue + " " + bugsTXT;
-	    
-	    // doesn't work, passes 0 for scoreValue instead of bug score
-    }
+        // Retrieve the score from PlayerPrefs
+        int scoreValue = PlayerPrefs.GetInt("Score", 0);
+
+        textElement.text = textValue + scoreValue + " " + bugsTXT;
     }
 
     // Update is called once per frame
     void Update()
     {
-		if (Input.GetKey("up"))
-		{
-            scoreValue++;
-			textElement.text = textValue + scoreValue + " " + bugsTXT;
-		}
-	}
+        if (Input.GetKey("up"))
+        {
+            int scoreValue = PlayerPrefs.GetInt("Score", 0); // Retrieve the score from PlayerPrefs
+            scoreValue++; // Increment the score
+            textElement.text = textValue + scoreValue + " " + bugsTXT;
+        }
+    }
 }
