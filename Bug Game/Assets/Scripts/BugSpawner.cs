a37b7e18c42563b1dd42a78b245a32e1bbda9bf3
ormle <101660382+ormle@users.noreply.github.com>
2023-10-30 13:54:59 -0600

    Bee movement
    Implement bee paths to follow
    - Need to figure out bee asset rotation along paths however

--- Bug Game/Assets/Scripts/BugSpawner.cs
+++ Bug Game/Assets/Scripts/BugSpawner.cs
@@ -36,17 +36,36 @@ public class BugSpawner : MonoBehaviour
     private float spawnPos_UD_Y = 1070;   //Not a range, either + or -
     private float spawnRange_UD_X = 1450; //X ranges where they can spawn
 
+    /*
+     * Bee Routes
+     * Routes is an array of routes where each route is an array of
+     * paths which contain an array of control points (confusing yes)
+     * Paths are the sections of a route that a bee follows based on the 
+     * control points
+     * 5 different routes to randomly choose from
+     */
+    public Transform[] Routes;
+    public List<Transform> Paths;
+
 
     // Start is called before the first frame update
     void Start()
     {
         //Beetle
-        InvokeRepeating("SpawnRandomBeetle", startDelay, spawnInterval);
+        if (beetlePrefabs.Length > 0)
+        {
+            InvokeRepeating("SpawnRandomBeetle", startDelay, spawnInterval);
+        }
         //Ladybug
-        InvokeRepeating("SpawnRandomLadybug", startDelay, spawnInterval);
-        //Bee
-        InvokeRepeating("SpawnRandomBee", startDelay, spawnInterval);
-       
+        if (ladybugPrefabs.Length > 0)
+        {
+            InvokeRepeating("SpawnRandomLadybug", startDelay, spawnInterval);
+        }
+        if (beePrefabs.Length > 0)
+        {
+            //Bee
+            InvokeRepeating("SpawnRandomBee", startDelay, spawnInterval);
+        }
     }
 
     // Update is called once per frame
@@ -181,69 +200,41 @@ public class BugSpawner : MonoBehaviour
 
     void SpawnRandomBee()
     {
-        //0 = Spawn from top, 1 spawn from bottom
-        int randUpDown = Random.Range(0, 1);
-        float upDown = spawnPos_UD_Y;
-        if (randUpDown == 1)
-        {
-            upDown = spawnPos_UD_Y * -1;
-        }
-
-        // Randomly generate bug index
+        // Randomly generate bug index for skin
         int bugIndex = Random.Range(0, beePrefabs.Length);
 
-        // Randomly generate spawn location
-        Vector3 spawnPos = new Vector3(Random.Range(-spawnRange_UD_X, spawnRange_UD_X),
-            upDown, 0);
+        //Pick a random route to follow
+        int route = Random.Range(0, Routes.Length);
+        //Put all paths in an array to give to the movement script
+        Paths = new List<Transform>();
+        for (int i = 0; i < Routes[route].childCount; i++)
+        {
+            Paths.Add(Routes[route].GetChild(i));
+            //Debug.Log("Name: " + Paths[i].name);
+        }
 
-        // Calculate a random rotation angle between 88 and 92 degrees.
-        float randomRotation = Random.Range(88f, 92f);
+        //Get spawn position from location of first path
+
+        //Debug.Log("Something: " + Paths[0].GetChild(0).localPosition);
+        Vector2 spawnPos = Paths[0].GetChild(0).localPosition;
+        //Debug.Log("Name: " + Paths[0].GetChild(0).name);
+        //Debug.Log("SpawnPos: " + spawnPos);
 
         // Spawn bug with the calculated random rotation angle.
-        GameObject bug = Instantiate(beePrefabs[bugIndex], spawnPos, Quaternion.Euler(0f, 0f, randomRotation));
+        //Currently rotation angle is 270deg
+        GameObject bug = Instantiate(beePrefabs[bugIndex], spawnPos, Quaternion.Euler(0f, 0f, 270));
         bug.transform.SetParent(transform, false);
 
-        // Access the MoveForward script on the spawned bug and change its speed
-        MoveForward moveScript = bug.GetComponent<MoveForward>();
+        // Access the MoveForward script on the spawned bug and change its speed and spawnPos
+        MoveCurve moveScript = bug.GetComponent<MoveCurve>();
         if (moveScript != null)
         {
-            // Calculate a random speed variation between -0.3 and 0.3, adding it to the base speed of 0.9.
-            float speedVariation = Random.Range(-0.3f, 0.3f);
-            moveScript.speed = 0.9f + speedVariation;
+            // Calculate a random speed variation between 0.2 and 0.4, adding it to the base speed of 0.15.
+            float speedVariation = Random.Range(0.2f, 0.4f);
+            moveScript.Speed += speedVariation;
+            moveScript.Paths = Paths;    //Give the script the route
+            moveScript.bugPos = spawnPos;//Give script bug position
         }
-
-        Debug.Log(spawnPos);
-
-        // Start a coroutine to control bug movement
-        StartCoroutine(BeeMovement(bug, moveScript));
-    }
-
-    IEnumerator BeeMovement(GameObject bug, MoveForward moveScript)
-    {//Right now is a copy of LadyBugMovement as a skeleton/guide
-        // Move for a random duration between 2 and 9 seconds
-        float moveDuration = Random.Range(1f, 9f);
-        yield return new WaitForSeconds(moveDuration);
-
-        // Stop moving for a random duration between 3 and 9 seconds
-        float stopDuration = Random.Range(0.2f, 3f);
-        moveScript.speed = 0f;
-        yield return new WaitForSeconds(stopDuration);
-
-        //if statement is fix for accessing deleted object error - yron
-
-        if (bug)
-        {
-            // Rotate to a random angle between 0 and 270 degrees
-            float randomRotation = Random.Range(0f, 270f);
-            bug.transform.rotation = Quaternion.Euler(0f, 0f, randomRotation);
-        }
-
-        // Resume moving with a random speed between 0.9 and 1.6
-        float randomSpeed = Random.Range(0.9f, 1.6f);
-        moveScript.speed = randomSpeed;
-
-        // You can repeat this cycle as needed
-        StartCoroutine(BeeMovement(bug, moveScript));
     }
     
 
