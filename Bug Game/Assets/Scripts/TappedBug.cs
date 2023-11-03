19ff2fe012329d4457194578cdcc2d4d62885b96
ormle <101660382+ormle@users.noreply.github.com>
2023-11-03 13:44:48 -0600

    Prelim Beetle fade

--- Bug Game/Assets/Scripts/TappedBug.cs
+++ Bug Game/Assets/Scripts/TappedBug.cs
@@ -46,6 +46,11 @@ public class TappedBug : MonoBehaviour
                 boinkAudioSource.Play();
             }
 
+            if (gameObject.name.Contains("TigerBeetle"))
+            {
+                gameObject.GetComponent<Animator>().Play("TB_FadeOut");
+            }
+
             Destroy(gameObject);
         }
     }
