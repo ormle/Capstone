1cd5f01b6b25f98c4d29e8b79785a4a9924487c5
yronk9 <geronay@mymacewan.ca>
2023-11-24 14:37:55 -0700

    ladybug + ant movement

--- Bug Game/Assets/Scripts/BugSpawner.cs
+++ Bug Game/Assets/Scripts/BugSpawner.cs
@@ -76,6 +76,12 @@ public class BugSpawner : MonoBehaviour
             new WaitForSeconds(startDelay);
             StartCoroutine("SpawnRandomLadybug", spawnInterval - 0.25);
         }
+	//ant
+	if (beetlePrefabs.Length > 0)
+        {
+            new WaitForSeconds(startDelay);
+            StartCoroutine("SpawnRandomAnt", spawnInterval - 0.35f);
+        }
         if (beePrefabs.Length > 0)
         {
             new WaitForSeconds(startDelay);
@@ -164,7 +170,7 @@ public class BugSpawner : MonoBehaviour
             if (moveScript != null)
             {
                 // Calculate a random speed variation between -0.3 and 0.3, adding it to the base speed of 0.9.
-                float speedVariation = Random.Range(-0.3f, 0.3f);
+                float speedVariation = Random.Range(0.3f, 1.3f);
                 moveScript.speed = 0.9f + speedVariation;
                 //Set so it moves towards screen depending on side its spawned on
                 moveScript.leftright = lr;
@@ -173,18 +179,65 @@ public class BugSpawner : MonoBehaviour
             //Debug.Log(spawnPos);
 
             // Start a coroutine to control bug movement
-            StartCoroutine(LadybugMovement(bug, moveScript));
+            StartCoroutine(BeetleMovement(bug, moveScript));
         }
     }
     
-    // USES Ladybug Coroutine!
+    IEnumerator SpawnRandomAnt(int repeat) // uses bee prefabs atm
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(repeat);
+            // Randomly generate bug index
+            int bugIndex = Random.Range(0, beePrefabs.Length);
+
+            //Randomly choose to spawn from left or right
+            int lr = Random.Range(0, 2);//0 = Left, 1 = Right
+            // Calculate a random rotation angle between 88 and 92 degrees.
+            float randomRotation;
+            if (lr == 0)
+            {
+                spawnPos_RL_X *= -1;
+                randomRotation = Random.Range(88f, 92f);
+            }
+            else { randomRotation = Random.Range(-88f, -92f); }
+
+            // Randomly generate spawn location
+            Vector3 spawnPos = new Vector3(spawnPos_RL_X,
+                Random.Range(-spawnRange_RL_YL, spawnRange_RL_YU), 0);
+
+            // Spawn bug
+            GameObject bug = Instantiate(beePrefabs[bugIndex], spawnPos,
+                Quaternion.Euler(0f, 0f, randomRotation));
+            bug.transform.SetParent(transform, false);
+
+            // Access the MoveForward script on the spawned bug and change its speed
+            MoveForward moveScript = bug.GetComponent<MoveForward>();
+            if (moveScript != null)
+            {
+                // Calculate a random speed variation between -0.3 and 0.3, adding it to the base speed of 0.9.
+                float speedVariation = Random.Range(0.3f, 0.8f);
+                moveScript.speed = 0.9f + speedVariation;
+                //Set so it moves towards screen depending on side its spawned on
+                moveScript.leftright = lr;
+            }
+
+            //Debug.Log(spawnPos);
+
+            // Start a coroutine to control bug movement
+            StartCoroutine(AntMovement(bug, moveScript));
+        }
+    }
+    
+    
+    // USES bee prefab Coroutine!
     IEnumerator SpawnRandomLadybug(int repeat)
     {
         while (true)
         {
             yield return new WaitForSeconds(repeat);
             // Randomly generate bug index
-            int bugIndex = Random.Range(0, beetlePrefabs.Length);
+            int bugIndex = Random.Range(0, beePrefabs.Length);
             //Randomly choose to spawn from left or right
             int lr = Random.Range(0, 1);//0 = Left, 1 = Right
             float randomRotation;
@@ -300,6 +353,62 @@ public class BugSpawner : MonoBehaviour
     
         // You can repeat this cycle as needed
         StartCoroutine(LadybugMovement(bug, moveScript));
+    }
+    
+    IEnumerator BeetleMovement(GameObject bug, MoveForward moveScript)
+    {
+        // Move for a random duration
+        float moveDuration = Random.Range(1f, 3f);
+        yield return new WaitForSeconds(moveDuration);
+
+        // Stop moving for a random duration 
+        float stopDuration = Random.Range(3f, 7f);
+        moveScript.speed = 0f;
+        yield return new WaitForSeconds(stopDuration);
+
+        //if statement is fix for accessing deleted object error - yron
+
+        if (bug)
+        { 
+        // Rotate to a random angle between 0 and 270 degrees
+        float randomRotation = Random.Range(0f, 270f);
+        bug.transform.rotation = Quaternion.Euler(0f, 0f, randomRotation);
+        }
+
+        // Resume moving with a random speed 
+        float randomSpeed = Random.Range(1.9f, 2.6f);
+        moveScript.speed = randomSpeed;
+    
+        // You can repeat this cycle as needed
+        StartCoroutine(BeetleMovement(bug, moveScript));
+    }
+    
+    IEnumerator AntMovement(GameObject bug, MoveForward moveScript)
+    {
+        // Move for a random duration
+        float moveDuration = Random.Range(2f, 3f);
+        yield return new WaitForSeconds(moveDuration);
+
+        // Stop moving for a random duration 
+        float stopDuration = Random.Range(1f, 2f);
+        moveScript.speed = 0f;
+        yield return new WaitForSeconds(stopDuration);
+
+        //if statement is fix for accessing deleted object error - yron
+
+        if (bug)
+        { 
+        // Rotate to a random angle between 0 and 270 degrees
+        float randomRotation = Random.Range(0f, 270f);
+        bug.transform.rotation = Quaternion.Euler(0f, 0f, randomRotation);
+        }
+
+        // Resume moving with a random speed 
+        float randomSpeed = Random.Range(1.0f, 1.6f);
+        moveScript.speed = randomSpeed;
+    
+        // You can repeat this cycle as needed
+        StartCoroutine(AntMovement(bug, moveScript));
     }
 
     IEnumerator DragonflyMovement(GameObject bug, MoveZigZag moveScript, float originalRotation)
