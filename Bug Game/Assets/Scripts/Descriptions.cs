a3cb8f7b7ef121ba5045dfe1db3ef47ea3178fb2
yronk9 <geronay@mymacewan.ca>
2023-11-27 17:55:49 -0700

    End Screen button broken

--- Bug Game/Assets/Scripts/Descriptions.cs
+++ Bug Game/Assets/Scripts/Descriptions.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TouchScript.Gestures;
+using System;
 
 public class Descriptions : MonoBehaviour
 {
     public float moveSpeed = 5f; // Adjust the speed as needed
     private bool movingUp = false;
     private bool movingDown = false;
+    private bool isTapped = false;
+
+    void Start()
+    {
+        TapGesture tapGesture = this.GetComponent<TapGesture>();
+        tapGesture.Tapped += TapGesture_Tapped;
+    }
 
     void Update()
     {
@@ -35,33 +44,60 @@ public class Descriptions : MonoBehaviour
             MoveDown();
         }
     }
+    
+    void TapGesture_Tapped(object sender, EventArgs e)
+    {
+        Debug.Log("Tapped! Initiating MoveDown()");
+        movingUp = false;
+        movingDown = true;
+    }
+    
 
     void MoveUp()
     {
         // Move the object upward along the Y-axis
-        float newY = transform.GetComponent<RectTransform>().anchoredPosition.y + (Vector3.up.y * moveSpeed * Time.deltaTime);
-    
+        float newY = transform.position.y + (Vector3.up.y * moveSpeed * Time.deltaTime);
+
         // Set the maximum height (adjust this value as needed)
-        float maxHeight = -450f;
-    
-        // Check if the new position exceeds the maximum height
-        if (newY < maxHeight)
+        float maxHeight = 0f;
+
+        // Gradually decrease the value of newY
+        newY = Mathf.Lerp(transform.position.y, maxHeight, Time.deltaTime * moveSpeed);
+
+        // Apply the new position
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+
+        // Check if the object has reached the maximum height
+        if (newY >= maxHeight)
         {
-            // Limit the position to the maximum height
-            newY = maxHeight;
+            // Stop moving up
+            movingUp = false;
             moveSpeed = 0;
         }
-    
-        // Apply the new position
-        transform.GetComponent<RectTransform>().anchoredPosition = new Vector2(transform.GetComponent<RectTransform>().anchoredPosition.x, newY);
     }
 
-
-    
-
-    void MoveDown()
-    {
+    public void MoveDown()
+    {   
+        //Debug.Log("moving down! Initiating MoveDown()");
         // Move the object downward along the Y-axis
-        transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+        float newY = transform.position.y + (Vector3.down.y * moveSpeed * Time.deltaTime);
+
+        // Set the minimum height (adjust this value as needed)
+        float minHeight = -10f;
+
+        // Gradually increase the value of newY
+        newY = Mathf.Lerp(transform.position.y, minHeight, Time.deltaTime * moveSpeed);
+
+        // Apply the new position
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+
+        // Check if the object has reached the minimum height
+        if (newY <= minHeight)
+        {
+            // Stop moving down
+            movingDown = false;
+            moveSpeed = 0;
+        }
     }
+
 }
